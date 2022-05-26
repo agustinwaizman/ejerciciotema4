f9d9601d178dda1eb766dc8f226a2396d29d8a43
Agustin <105565517+agustinwaizman@users.noreply.github.com>
2022-05-26 18:06:18 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+// resolucion ejercicio sesion 4
+    public static void main(String[] args) {
+        int numeroIf = -5;
+        if (numeroIf < 0){
+            System.out.println("es un numero negativo");
+        } else if (numeroIf > 0){
+            System.out.println("es un numero positivo");
+        } else {
+            System.out.println("el numero es 0");
+        }
+    }
+    public static void numWhile(String[] args){
+        int numeroWhile = 0;
+        while(numeroWhile < 3){
+            System.out.println(numeroWhile);
+            numeroWhile++;
+        }
+    }
+    public static void numDoWhile(String[] args){
+        int numeroDoWhile = 2;
+        do{
+            numeroDoWhile++;
+            System.out.println(numeroDoWhile);
+        }while(numeroDoWhile < 3);
+    }
+    public static void numFor(String[] args){
+        for (int numeroFor= 1; numeroFor <= 3; numeroFor++){
+            System.out.println(numeroFor);
+        }
+    }
+    public static void numSwitch(String[] args){
+        var estacion = "INVIERNO";
+        switch(estacion){
+            case "VERANO": System.out.println("es verano"); break;
+            case "OTOÑO": System.out.println("es otoño"); break;
+            case "INVIERNO": System.out.println("es invierno"); break;
+            case "PRIMAVERA": System.out.println("es primavera"); break;
+            default: System.out.println("no es una estacion...");
+        }
+    }
+}
